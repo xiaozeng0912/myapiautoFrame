81eecaeeb02f9f646f6207d9712feb544b6bcc22
xiaozeng0912 <1184556804.com>
2025-12-10 18:32:16 +0800

    修改run.py

--- myApi/run.py
+++ myApi/run.py
@@ -5,4 +5,4 @@ import pytest
 if __name__ == '__main__':
     pytest.main(['-s', '-v', '--alluredir=./output/report/temp', './testcase', '--clean-alluredir'])
     shutil.copy('./environment.xml', './output/report/temp')
-    os.system(f'allure serve ./output/report/temp')
+    #os.system(f'allure serve ./output/report/temp')
